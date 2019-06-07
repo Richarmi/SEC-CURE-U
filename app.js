5b29495cbfa4472e972405885de13b3d037bccd7
mtric <Mtrichards@outlook.com>
2019-06-07 15:15:44 -0600

    added a middleware function to log every requests made to the server

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ var path = require('path');
 app.set('port', 3000);
 assignedPort = app.get('port');
 
+//
+app.use(function(req, res, next){
+    console.log(req.method, req.url);
+    next();
+});
+
 // declare the "public" folder as static
 app.use(express.static(path.join(__dirname, "public")));
 /*---------------------------------------------------------------*/
